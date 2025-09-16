79da87d403ef38807d18b5cc3737e2c0e5dd1951
Lucky Tiwari <144935714+luckyarsd@users.noreply.github.com>
2025-09-16 16:32:35 +0530

    Update script.js

--- script.js
+++ script.js
@@ -3,15 +3,6 @@
 // ---------------------------------------------
 
 // Replace with your Firebase config
-const firebaseConfig = {
-    apiKey: "AIzaSyDYYx46KEkHSA78xHMo8lYS8tKqYrLZK-w",
-    authDomain: "skillpoint-c3e39.firebaseapp.com",
-    projectId: "skillpoint-c3e39",
-    storageBucket: "skillpoint-c3e39.firebasestorage.app",
-    messagingSenderId: "984664107678",
-    appId: "1:984664107678:web:af02e53e532d4e57f0298f",
-    measurementId: "G-390K2B4HC7"
-};
 
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
